cf38c9fb6489f7270df024f88a8a71c4da9963ce
James <wu.james@hotmail.com>
2021-11-05 15:02:44 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+/**
+ * movie searcher
+ *  
+ * landing page with form consisting of 1 drop down menu for genres and a search bar
+ *   
+ * retrieve information once form is submitted
+ *  
+ * store names and img of movie cover
+ *  
+ * display information page
+ *  
+ * clicking on movie will redirect to imdb
+ *  
+ * STRETCH GOAL
+ * use ajax to add more elements to the page as scrolling down page
+ *  
+ */
+
+
+
