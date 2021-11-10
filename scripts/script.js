932ce392d4d59b94b7e973b63026ea3d034556d4
James Wu <wu.james@hotmail.com>
2021-11-10 17:48:20 -0500

    js changes

--- scripts/script.js
+++ scripts/script.js
@@ -7,16 +7,19 @@ const moviesApp = {};
 // api info
 moviesApp.apiKey = 'k_bsagv4oc';
 moviesApp.apiKey = 'k_af7vd4w1';
-moviesApp.apiUrl = 'https://imdb-api.com/en/API/SearchMovie/';
+moviesApp.baseUrl = 'https://imdb-api.com/';
+moviesApp.searchMovieEndPoint = 'en/API/SearchMovie/';
+moviesApp.keywordEndPoint = 'en/API/SearchKeyword/';
 
+// let a = `${moviesApp.apiUrl}${apiKey}`
 
 // getting info from the API
 moviesApp.getMovies = (movie) => { 
-
-    const url = new URL(moviesApp.apiUrl);
+    console.log(movie);
+    const url = new URL(`${moviesApp.baseUrl}${moviesApp.searchMovieEndPoint}`);
     url.search = new URLSearchParams({
         apiKey: moviesApp.apiKey,
-        expression: ""
+        expression: movie
         // this expression is where the user types in the search
     })
 
@@ -24,61 +27,65 @@ moviesApp.getMovies = (movie) => {
     fetch(url)
         .then((response) => {
             return response.json();
-            console.log(response)
         })
         .then((data) => {
-            moviesApp.displayMovies(data);
+            // moviesApp.displayMovies(data);
+            console.log(data)
         })
         // data is the json response 
 }
 
 
 // function to display the info on the page
-    moviesApp.displayMovies = (data) => {
+moviesApp.displayMovies = (data) => {
 
-    // <div class="movies">
-    //    <ul>
-    //       <h2>title of movie</h2>
-    //       <img src="" alt="">
-    //    </ul>
-    // </div>
+// <div class="movies">
+//    <ul>
+//       <h2>title of movie</h2>
+//       <img src="" alt="">
+//    </ul>
+// </div>
 
-        data.results.forEach( (item) => {
-        // creating elements to display on page
-        // for (let i = 0; i < 10; i++)
+    // data.results.forEach( (item) => {
+    // // creating elements to display on page
+    // // for (let i = 0; i < 10; i++)
 
-        const movieDiv = document.createElement('div')
-        movieDiv.classList.add("movies")
-        
-        const movieTitle = document.createElement('h2')
-        movieTitle.innerText = `${item.title}`
+    // const movieDiv = document.createElement('div')
+    // movieDiv.classList.add("movies")
+    
+    // const movieTitle = document.createElement('h2')
+    // movieTitle.innerText = `${item.title}`
 
-		const movieImg = document.createElement('img')
-        movieImg.src = `${item.image}`
-        movieImg.alt = `${item.title}`
+    // const movieImg = document.createElement('img')
+    // movieImg.src = `${item.image}`
+    // movieImg.alt = `${item.title}`
 
 
-        movieDiv.appendChild(movieTitle)
-        movieDiv.appendChild(movieImg)
-        })
-    }
+    // movieDiv.appendChild(movieTitle)
+    // movieDiv.appendChild(movieImg)
+    // })
+}
 
 // attach event listeners to the page
-    moviesApp.setupEventListeners = () => {
-        document.getElementById("movieGenre").addEventListener("change", (event) => {
-            const genreChoice = event.target.value
+moviesApp.setupEventListeners = () => {
+    // document.getElementById("movieGenre").addEventListener("change", (event) => {
+    //     const genreChoice = event.target.value
 
-        })
-    }
+    // })
+
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const userInput = document.getElementById('text').value;
+        moviesApp.getMovies(userInput);
+    })
+
+}
 
 // init/calls functions
-    moviesApp.init = () => {
-        moviesApp.getMovies()
-        moviesApp.setupEventListeners()
-    }
+moviesApp.init = () => {
+    moviesApp.getMovies("string")
+    moviesApp.setupEventListeners()
+}
 
 // initializing app 
-    moviesApp.getMovies();
-
-
-
+moviesApp.init();
